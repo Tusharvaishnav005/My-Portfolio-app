4adc66ab4ce728702a4fe1bef6049aaca41aaa45
Tushar Vaishnav <135534368+Tusharvaishnav005@users.noreply.github.com>
2025-07-29 17:06:15 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "Welcome to Tushar's portfolio, I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Tusharvaishnav005",
   resume:
-    "https://drive.google.com/file/d/1htaN8c7TEnuXsvC8EEMT5z9C4cMQM-gQ/view?usp=sharing",
+    "https://drive.google.com/file/d/1pN-cOxxo_aJurYamPtyB-Vq5LnajSVHz/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/tushar-vaishnav-56a090239/",
   github: "https://github.com/Tusharvaishnav005",
   insta: "https://www.instagram.com/t_vaishnav.005/",
